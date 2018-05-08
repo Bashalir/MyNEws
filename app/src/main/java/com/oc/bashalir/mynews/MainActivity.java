4f166ab69fcf103d560e9b07351b4cc495529660
Bashalir <bachir@sahali.fr>
2018-05-08 02:06:23 +0200

    test

--- app/src/main/java/com/oc/bashalir/mynews/MainActivity.java
+++ app/src/main/java/com/oc/bashalir/mynews/MainActivity.java
@@ -3,8 +3,10 @@ package com.oc.bashalir.mynews;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+
+public class MainActivity extends AppCompatActivity {
+    private String test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
